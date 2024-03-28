332663cea6ef485a2d49ea5d56e14fa7ef1b207d
Mathangi Pushparajah <mathangi23@outlook.com>
2024-03-28 17:38:11 +0100

    Fixed greeting and added calculateDays

--- src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -3,10 +3,19 @@ package se.salt.precourse.firstjavaapp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Greeter {
+
   private static String greet(String namePassedIn) {
-    return "Welcome to SALT, ";
+    return "Welcome to SALT, " + namePassedIn;
+  }
+
+  private static long calculateDays(String date){
+    LocalDate currentDate = LocalDate.now();
+    long days = ChronoUnit.DAYS.between(currentDate,LocalDate.parse(date));
+    return days;
   }
 
   public static void main(String[] args) throws IOException {
@@ -15,8 +24,10 @@ public class Greeter {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
 
-    String greeting = greet(name);
+    System.out.print("When does that course start? ");
+    String date =reader.readLine();
 
-    System.out.println(greeting);
+    System.out.println(greet(name));
+    System.out.println("Today it is "+ calculateDays(date) +" days left until the course starts");
   }
 }
