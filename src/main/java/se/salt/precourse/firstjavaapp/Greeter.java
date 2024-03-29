2c9f0b849c72c10dbab4a12efe026423dfcb155b
Mathangi Pushparajah <mathangi23@outlook.com>
2024-03-29 13:48:31 +0100

    Added sprint boot

--- src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -1,18 +1,29 @@
 package se.salt.precourse.firstjavaapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
-public class Greeter {
+@SpringBootApplication
+public class Greeter implements CommandLineRunner {
+
+  @Autowired StartDateHandler startDateHandler;
 
   private static String greet(String namePassedIn) {
     return "Welcome to SALT, " + namePassedIn;
   }
 
   public static void main(String[] args) throws IOException {
+    SpringApplication.run(Greeter.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     System.out.print("What is your name? ");
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,12 +33,10 @@ public class Greeter {
     System.out.print("When does that course start (yyyy-mm-dd)? ");
     String startDate = reader.readLine();
 
-    StartDateHandler startDateHandler = new StartDateHandler();
     if(startDateHandler.dateHasOnlyNumbers(startDate) ){
       System.out.print("You start in " + startDateHandler.daysToCourseStart(startDate) + " days. Better get cracking on that Pre-course!!");
     } else {
       System.out.print("Please provide a valid date");
     }
-
   }
 }
