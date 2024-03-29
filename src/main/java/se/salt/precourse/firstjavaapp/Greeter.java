e21cea2f7621e2e5d6cf8acd4afe015f4e7a9a4f
Mathangi Pushparajah <mathangi23@outlook.com>
2024-03-29 12:44:21 +0100

    added StartDateHandler class, edited Greeter class to use StartDateHandler, and added plugin and dependency to pom file

--- src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -12,22 +12,22 @@ public class Greeter {
     return "Welcome to SALT, " + namePassedIn;
   }
 
-  private static long calculateDays(String date){
-    LocalDate currentDate = LocalDate.now();
-    long days = ChronoUnit.DAYS.between(currentDate,LocalDate.parse(date));
-    return days;
-  }
-
   public static void main(String[] args) throws IOException {
     System.out.print("What is your name? ");
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
-
-    System.out.print("When does that course start? ");
-    String date =reader.readLine();
-
     System.out.println(greet(name));
-    System.out.println("Today it is "+ calculateDays(date) +" days left until the course starts");
+
+    System.out.print("When does that course start (yyyy-mm-dd)? ");
+    String startDate = reader.readLine();
+
+    StartDateHandler startDateHandler = new StartDateHandler();
+    if(startDateHandler.dateHasOnlyNumbers(startDate) ){
+      System.out.print("You start in " + startDateHandler.daysToCourseStart(startDate) + " days. Better get cracking on that Pre-course!!");
+    } else {
+      System.out.print("Please provide a valid date");
+    }
+
   }
 }
